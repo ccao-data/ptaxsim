39e78ce7c7b67178c4fe703c8c7558a28ad7ffc1
Dan Snow <snow.dfs@gmail.com>
2022-07-08 21:18:57 +0000

    Cleanup equalization factor table

--- data-raw/eq_factor/eq_factor.R
+++ data-raw/eq_factor/eq_factor.R
@@ -3,17 +3,17 @@ library(dplyr)
 library(readr)
 
 remote_bucket <- Sys.getenv("S3_REMOTE_BUCKET")
-remote_path <- file.path(remote_bucket, "eq_factors", "part-0.parquet")
+remote_path <- file.path(remote_bucket, "eq_factor", "part-0.parquet")
 
-eq_factors <- readr::read_csv(
-  file = "data-raw/eq_factors/eq_factors.csv",
+eq_factor <- readr::read_csv(
+  file = "data-raw/eq_factor/eq_factor.csv",
   col_types = cols(year = col_character(), equalization_factor = col_double())
 ) %>%
   arrange(year)
 
 # Write to S3
 arrow::write_parquet(
-  x = eq_factors,
+  x = eq_factor,
   sink = remote_path,
   compression = "zstd"
 )
