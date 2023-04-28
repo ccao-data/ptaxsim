02c569b8672df9114b72c8ac32179365667c6434
Dan Snow <daniel.snow@cookcountyil.gov>
2023-04-28 19:51:15 +0000

    Add SQL for PIN geometries

---
+++ data-raw/create_db_geometry.sql
@@ -0,0 +1,21 @@
+/** pin_geometry_raw **/
+CREATE TABLE pin_geometry_raw (
+    pin10                   varchar(10)                                NOT NULL,
+    start_year              int                                        NOT NULL,
+    end_year                int                                        NOT NULL,
+    longitude               double                                     NOT NULL,
+    latitude                double                                     NOT NULL,
+    geometry                text                                       NOT NULL,
+    PRIMARY KEY (pin10, start_year, end_year)
+) WITHOUT ROWID;
+
+
+/** pin_geometry **/
+CREATE VIEW IF NOT EXISTS pin_geometry AS
+SELECT y.year, pr.pin10, pr.longitude, pr.latitude, pr.geometry
+FROM (
+  SELECT DISTINCT year
+  FROM agency
+) AS y
+CROSS JOIN pin_geometry_raw pr
+ON y.year >= start_year AND y.year <= end_year
