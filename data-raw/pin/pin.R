bf3a73a4190a12df23daa8a71e892d0e099f1539
Dan Snow <daniel.snow@cookcountyil.gov>
2023-04-28 18:49:59 +0000

    Add script to generate collapsed PIN geometries

--- data-raw/pin/pin.R
+++ data-raw/pin/pin.R
@@ -3,12 +3,17 @@ library(DBI)
 library(dplyr)
 library(geoarrow)
 library(odbc)
+library(sf)
 library(tidyr)
 
 # Paths for remote storage on S3
 remote_bucket <- Sys.getenv("S3_REMOTE_BUCKET")
 remote_path_pin <- file.path(remote_bucket, "pin")
-remote_path_pin_geometry <- file.path(remote_bucket, "pin_geometry")
+remote_path_pin_geometry_raw <- file.path(
+  remote_bucket,
+  "pin_geometry_raw",
+  "part-0.parquet"
+)
 
 
 
@@ -114,16 +119,48 @@ arrow::write_dataset(
 agency_df <- read_parquet(file.path(remote_bucket, "agency", "part-0.parquet"))
 max_year <- max(as.integer(agency_df$year))
 
-
+# Grab parcel shapes from S3. These files are originally from the data portal
+# and Cook Central
 remote_bucket_geometry <- "s3://ccao-data-warehouse-us-east-1/spatial/parcel"
-pin_geometry_df_raw <- arrow::open_dataset(remote_bucket_geometry) %>%
-  filter(year >= 2006 & year < max_year) %>%
-  select(year, pin10, longitude = lon, latitude = lat, geometry) %>%
+pin_geometry_df_full <- arrow::open_dataset(remote_bucket_geometry) %>%
+  filter(year >= 2006 & year <= max_year) %>%
+  select(year, x = lon, y = lat, pin10, geometry) %>%
   geoarrow_collect_sf()
 
-pin_geometry_df_raw %>%
-  sf::st_drop_geometry() %>%
-  count(year)
+# For each PIN10, keep only records where the shape/area of the PIN have changed
+# and record the start and end year for each unique shape/area
+pin_geometry_df_raw <- pin_geometry_df_full %>%
+  mutate(area = st_area(geometry)) %>%
+  group_by(pin10) %>%
+  arrange(pin10, year) %>%
+  mutate(across(c(area, x, y), lag, .names = "lag_{.col}")) %>%
+  mutate(
+    diff_area = !(abs(area - lag_area) < units::set_units(0.001, "m^2")),
+    diff_cent = !(abs(x - lag_x) < 0.00001 & abs(y - lag_y) < 0.00001),
+    pin_group = cumsum(
+      (diff_area & diff_cent) |
+        (is.na(diff_area) & is.na(diff_cent))
+    )
+  ) %>%
+  group_by(pin10, pin_group) %>%
+  mutate(
+    start_year = min(year),
+    end_year = max(year)
+  ) %>%
+  filter(row_number() == 1) %>%
+  ungroup() %>%
+  select(pin10, start_year, end_year, longitude = x, latitude = y, geometry) %>%
+  arrange(pin10, start_year)
+
+# Write the raw PIN geometry to S3
+geoarrow::write_geoparquet(
+  pin_geometry_df_raw,
+  sink = remote_path_pin_geometry_raw,
+  compression = "zstd"
+)
+
+
+
 
 
 test_db <- DBI::dbConnect(RSQLite::SQLite(), "test-db.db")
