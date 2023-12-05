0f4622c9e9d021cc48167a3f41b7d21d3e8f4417
Eric Langowski <33432469+erhla@users.noreply.github.com>
2023-12-05 14:18:46 -0600

    Switching to pdftools
    Pretty sure this is the same but didn't want to use noncran tabulizer

--- data-raw/cpi/cpi.R
+++ data-raw/cpi/cpi.R
@@ -1,7 +1,8 @@
 library(arrow)
 library(dplyr)
 library(miniUI)
-library(tabulizer)
+#library(tabulizer)
+library(pdftools)
 library(tidyr)
 library(stringr)
 
@@ -14,27 +15,38 @@ row_to_names <- function(df) {
 # The goal of this script is to create a data frame of Consumer Price Indices
 # CPI-U used by PTELL to calculate/cap property tax extensions
 # We can load the historical CPIs from a PDF provided by the State of Illinois
+#2022 https://tax.illinois.gov/content/dam/soi/en/web/tax/localgovernments/property/documents/cpihistory.pdf
 
 # Paths for local raw data storage and remote storage on S3
 remote_bucket <- Sys.getenv("S3_REMOTE_BUCKET")
 remote_path <- file.path(remote_bucket, "cpi", "part-0.parquet")
 
 # Extract the table only (no headers), then manually assign header
-cpi_ext <- extract_areas(file = "data-raw/cpi/cpihistory.pdf")[[1]]
-cpi <- as_tibble(cpi_ext[, c(1, 2, 4, 5, 6)])
-cpi <- setNames(cpi, c("year", "cpi", "ptell_cook", "comments", "levy_year"))
+#cpi_ext <- extract_areas(file = "data-raw/cpi/cpihistory.pdf")[[1]]
+
+cpi <- pdftools::pdf_text(pdf = "data-raw/cpi/cpihistory.pdf") %>%
+  str_extract(., regex('1991.*', dotall = TRUE)) %>%
+  str_remove_all(., "\\(5 % for Cook\\)") %>%
+  str_split(., '\n') %>%
+  unlist() %>%
+  tibble(vals = `.`) %>%
+  mutate(vals = str_squish(vals)) %>%
+  separate_wider_delim(col=vals,
+                       names=c("year", "cpi", "pct", "ptell_cook", "levy_year", "year_paid"),
+                       delim = ' ', too_few='align_start', too_many = "drop")
+
+#cpi <- as_tibble(cpi_ext[, c(1, 2, 4, 5, 6)])
+#cpi <- setNames(cpi, c("year", "cpi", "ptell_cook", "comments", "levy_year"))
 
 # Merge Cook rate into main column
 cpi <- cpi %>%
   mutate(
     across(c(year, levy_year), as.character),
     across(c(cpi), as.numeric),
-    across(c(ptell_cook, comments), readr::parse_number),
-    ptell_cook = ifelse(!is.na(comments), comments, ptell_cook),
+    across(c(ptell_cook), readr::parse_number),
     ptell_cook = ptell_cook / 100
   ) %>%
-  select(-comments) %>%
-  filter(year != "1991") %>%
+  filter(year != "1991", year != "", year != 'CPI') %>%
   arrange(year)
 
 # Write to S3
