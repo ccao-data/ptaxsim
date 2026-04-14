4a444230ebbf7a4455939d55bc3565d11bfbe81f
kyrasturgill <kyra.sturgill@cookcountyil.gov>
2026-04-14 21:43:28 +0000

    Add lim_denominator test back in, fix pre-commit error

--- data-raw/create_db.sql
+++ data-raw/create_db.sql
@@ -8,7 +8,7 @@ CREATE TABLE agency (
     home_rule_ind           boolean                                    NOT NULL,
     agg_ext_base_year       int      CHECK(agg_ext_base_year >= 2003)          ,
     lim_numerator           bigint   CHECK(lim_numerator >= 0)                 ,
-    lim_denominator         bigint                                             ,
+    lim_denominator         bigint   CHECK(lim_denominator > 0)                ,                      ,
     lim_rate                double   CHECK(lim_rate >= 0)                      ,
     prior_eav               bigint   CHECK(prior_eav >= 0)                     ,
     curr_new_prop           bigint                                     NOT NULL,
@@ -259,4 +259,3 @@ CREATE INDEX ix_pin_tif_distribution_year_agency_num
     ON pin_tif_distribution(year, agency_num);
 CREATE INDEX ix_pin_tif_distribution_year_tax_code_num
     ON pin_tif_distribution(year, tax_code_num);
-
