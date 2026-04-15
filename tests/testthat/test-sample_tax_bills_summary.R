e984b74801f3b06e2653807ad66e58ff12ea776c
Jean Cochrane <jean@jeancochrane.com>
2026-04-15 10:45:06 -0500

    Update `test-sample_tax_bills_summary.R` so that tests can run on built package

--- tests/testthat/test-sample_tax_bills_summary.R
+++ tests/testthat/test-sample_tax_bills_summary.R
@@ -2,34 +2,35 @@ context("Test sample_tax_bills_summary")
 
 ##### TEST sample_tax_bills_summary #####
 
-library(data.table)
 library(dplyr)
-library(here)
-library(readr)
-library(stringr)
 
-test_that("sample_tax_bills_summary PIN-years match sample_tax_bill PDFs", {
-  pdf_dir <- here("data-raw", "sample_tax_bills")
-  if (!dir.exists(pdf_dir)) stop("PDF directory not found: ", pdf_dir)
+ptaxsim_db_conn <- DBI::dbConnect(
+  RSQLite::SQLite(),
+  Sys.getenv("PTAXSIM_DB_PATH")
+)
+assign("ptaxsim_db_conn", ptaxsim_db_conn, envir = .GlobalEnv)
 
-  pdf_pin_years <- list.files(
-    pdf_dir,
-    pattern = "\\.pdf$",
-    full.names = FALSE
+# Test to make sure we have added sample tax bills for the latest year
+test_that("max data year matches max year for sample tax bills", {
+  max_year <- DBI::dbGetQuery(
+    ptaxsim_db_conn,
+    "SELECT data_year_max FROM metadata"
   ) %>%
-    # Assume filenames are structured like "<year>_<class>_<PIN>.pdf"
-    tibble(filename = .) %>%
-    mutate(
-      year = as.integer(str_extract(filename, "^[0-9]{4}")),
-      pin = str_extract(filename, "[0-9]{14}")
-    ) %>%
-    select(year, pin) %>%
+    pull()
+
+  expect_equal(max(sample_tax_bills_summary$year), max_year)
+  expect_equal(max(sample_tax_bills_detail$year), max_year)
+})
+
+# Test to make sure we have updated both the summary and detail sample bills
+test_that("sample_tax_bills_summary PIN-years match sample_tax_bills_detail", {
+  summary_pin_years <- sample_tax_bills_summary %>%
+    distinct(year, pin) %>%
     arrange(year, pin)
 
-  summary_pin_years <- sample_tax_bills_summary %>%
-    select(year, pin) %>%
-    arrange(year, pin) %>%
-    as_tibble()
+  detail_pin_years <- sample_tax_bills_detail %>%
+    distinct(year, pin) %>%
+    arrange(year, pin)
 
-  expect_equal(pdf_pin_years, summary_pin_years, ignore_attrs = TRUE)
+  expect_equal(summary_pin_years, detail_pin_years, ignore_attr = TRUE)
 })
