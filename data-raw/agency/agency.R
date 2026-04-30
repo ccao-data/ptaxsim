4f57fe33566659f49a60d85c9a2b2c5bb9b00f13
Jean Cochrane <jean@jeancochrane.com>
2026-04-30 13:59:12 -0500

    Bring back exclusion for Skokie Library fund when constructing agency crosswalks in `agency.R`

--- data-raw/agency/agency.R
+++ data-raw/agency/agency.R
@@ -609,7 +609,12 @@ agency_crosswalk <-
     agency_num_final = agency
   ) %>%
   distinct() %>%
-  filter(agency_num != agency_num_final)
+  filter(agency_num != agency_num_final) %>%
+  # Account for error in Clerk's report which lists the Skokie Library fund
+  # levy in the legacy fund line. Because of this, it continues to follow the
+  # pre-2024 pattern for library fund reporting, so we need to remove it from
+  # the crosswalk
+  filter(!(agency_num == "031170001" & agency_num_24 == "031170000"))
 
 
 # agency_fund_crosswalk --------------------------------------------------------
